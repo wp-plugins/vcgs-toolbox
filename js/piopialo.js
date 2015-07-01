c390cd9cf870d63b8df8f4ab3f9d4dd3f2045e25
vcgs <vcgs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-01 10:25:47 +0000

    Version 1.9.6.1
    git-svn-id: https://plugins.svn.wordpress.org/vcgs-toolbox/tags/1.9.6.1@1190694 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/piopialo.js
@@ -0,0 +1,67 @@
+// JavaScript Document
+function oWDL (objeto) {
+	window.open(objeto.attr('data-piolink'));
+}
+jQuery('[data-piolink]').click(function(event) {    
+  oWDL(jQuery(this));
+  event.preventDefault();
+});
+
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+jQuery(document).ready(function(e) {
+    if ( ! (typeof activate_selector === 'undefined')) {
+		 jQuery('article').mouseup(function (e){
+			 texto = getSelectionText();
+			 if (texto!= '')
+       		{
+				 creaPioton(e,texto);
+			}
+   		});
+		jQuery('article').mousedown(function (e) {
+				if (window.getSelection) {
+				  if (window.getSelection().empty) {  // Chrome
+					window.getSelection().empty();
+				  } else if (window.getSelection().removeAllRanges) {  // Firefox
+					window.getSelection().removeAllRanges();
+				  }
+				} else if (document.selection) {  // IE?
+				  document.selection.empty();
+				}
+				pioboton = document.getElementById('pioton');
+				if (pioboton) pioboton.remove();
+		});
+	}
+});
+
+function creaPioton(e,texto)
+{
+		var x = e.pageX + 'px';
+        var y = e.pageY + 'px';
+        var div = document.createElement('div');
+		div.id = 'pioton';
+		div.style.position = 'absolute';
+		div.style.left = x;
+		div.style.top = y;
+        div.innerHTML = TuitLink(texto);
+        document.body.appendChild(div);
+}
+
+function TuitLink(texto) {
+	// vamos a contar los caracteres que nos quedan para la frase
+	dispo = 140 - pioselector_via.length - 26;
+	if (texto.length > dispo) 
+	{
+		texto = texto.substring(0,dispo-4) + '...';
+	} 
+	texto = '"' + texto + '" ' + pioselector_via + ' ' + window.location.href;
+	link = '<a href="http://www.twitter.com/intent/tweet/?text=' + encodeURIComponent(texto) + '" target="_blank"><i class="fa fa-twitter"></i> ' + pioselector_llamada + '</a>';
+	return link;
+}
